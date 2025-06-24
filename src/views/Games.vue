2f90bfd810ec696e7f10a0f6a5519bd419ef9448
ken7python <active.ken.315@gmail.com>
2025-06-24 16:00:54 +0900

    Add image of games in game page

--- src/views/Games.vue
+++ src/views/Games.vue
@@ -5,27 +5,30 @@
   class Game {
     name: string;
     url: string;
+    imgPath: string | null = null;
 
-    constructor(name: string, url: string) {
+    constructor(name: string, url: string, imgPath: string | null) {
       this.name = name;
       this.url = url;
+      this.imgPath = imgPath;
     }
   }
 
+  let imgPath :string = "./gameimg";
   let games = ref<Game[]>([]);
   games.value = [
-    new Game('キッキングスナイパー', 'https://unityroom.com/games/kencode_kickgame'),
-    new Game('Tank-Rush', 'https://unityroom.com/games/kencode_tankrush'),
-    new Game('スカイバルーンアドベンチャー', 'https://unityroom.com/games/kencode_freefall/'),
-    new Game('デジタル絵合わせ', 'https://ken7python.github.io/Eawase'),
-    new Game('弾幕シューティング', 'http://ken7python.github.io/biba/'),
-    new Game('オセロ', 'http://ken7python.github.io/Osero/'),
-    new Game('勝山恐竜めぐり', 'http://ken7python.github.io/DinosaurTour/'),
-    new Game('回り将棋', 'http://ken7python.github.io/MawariShogi/'),
-    new Game('県庁所在地クイズ', 'http://ken7python.github.io/Todouhuken/'),
-    new Game('1-9パズル', 'http://ken7python.github.io/pazuru/'),
-    new Game('迷路3', 'https://unityroom.com/games/kencode_maze3'),
-    new Game('Bring', 'https://unityroom.com/games/kencode_bring')
+    new Game('キッキングスナイパー', 'https://unityroom.com/games/kencode_kickgame',`${imgPath}/KickGame.png`),
+    new Game('Tank-Rush', 'https://unityroom.com/games/kencode_tankrush',`${imgPath}/TankRush.png`),
+    new Game('スカイバルーンアドベンチャー', 'https://unityroom.com/games/kencode_freefall/',`${imgPath}/SkyBalloonAdventure.png`),
+    new Game('デジタル絵合わせ', 'https://ken7python.github.io/Eawase',`${imgPath}/DigitalPictureMatching.png`),
+    new Game('弾幕シューティング', 'http://ken7python.github.io/biba/',`${imgPath}/BulletHellShooting.png`),
+    new Game('オセロ', 'http://ken7python.github.io/Osero/',`${imgPath}/Othello.png`),
+    new Game('勝山恐竜めぐり', 'http://ken7python.github.io/DinosaurTour/',`${imgPath}/KatsuyamaDinosaurTour.png`),
+    new Game('回り将棋', 'http://ken7python.github.io/MawariShogi/',`${imgPath}/MawariShogi.png`),
+    new Game('県庁所在地クイズ', 'http://ken7python.github.io/Todouhuken/',`${imgPath}/PrefecturalCapitalQuiz.png`),
+    new Game('1-9パズル', 'http://ken7python.github.io/pazuru/',`${imgPath}/1-9Puzzle.png`),
+    new Game('迷路3', 'https://unityroom.com/games/kencode_maze3',`${imgPath}/Maze3.png`),
+    new Game('Bring', 'https://unityroom.com/games/kencode_bring',`${imgPath}/Bring.png`)
   ];
 
 </script>
@@ -40,6 +43,7 @@
           <p class="gameName">
             {{ game.name }}
           </p>
+          <img v-if="game.imgPath" :src="game.imgPath" alt="Game Image" style="width: 100%; height: auto; border-radius: 8px;">
         </a>
       </div>
     </div>
