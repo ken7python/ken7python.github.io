acb89b9454e55cf8537323ab4aaeddf7c6480021
ken7python <active.ken.315@gmail.com>
2025-07-07 19:15:22 +0900

    Public to /

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,18 @@ import Home from '../views/Home.vue'
 import Blog from '../views/Blog.vue'
 import Game from '../views/Games.vue'
 import Repos from '../views/Repos.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
     { path: '/', component: Home, name: 'Home' },
     { path: '/blog', component: Blog, name: 'Blog' },
     { path: '/game', component: Game, name: 'Game' },
-    { path: '/repos', component: Repos, name: 'Repos' }
+    { path: '/repos', component: Repos, name: 'Repos'},
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFound
+    }
 ]
 
 const router = createRouter({
