253406ce4fd477e7fe51eacf14754c5d49783fb7
KenCode <66853112+ken7python@users.noreply.github.com>
2025-07-07 18:29:04 +0900

    Revert base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/',
-  base : '/portfolio/',
+  base: '/portfolio/',
   plugins: [vue()],
 })
