878291aeb2ddfb1106205ddcf072a7dd7cc6e99b
ken7python <active.ken.315@gmail.com>
2026-02-23 18:28:03 +0900

    Update: pages.kencode.tech

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -3,11 +3,22 @@
 </script>
 
 <template>
-  <h1>pages.kencode.tech</h1>
-  <p>KenCode(ken7python)のGitHub Pagesへようこそ</p>
-  <a href="https://kencode.tech">ポートフォリオサイト</a>
-  <br>
-  <a href="https://github.com/ken7python">GitHub</a>
-
-  <p>これから、このサイトを不定期で更新していきたいと思います</p>
+  <div id="main">
+    <h1>pages.kencode.tech</h1>
+    <p>KenCode(ken7python)のGitHub Pagesへようこそ</p>
+    <a href="https://kencode.tech">ポートフォリオサイト</a>
+    <br>
+    <a href="https://github.com/ken7python">GitHub</a>
+    <p>これから、このサイトを不定期で更新していきたいと思います</p>
+  </div>
 </template>
+
+<style>
+  #main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 80vh;
+  }
+</style>
